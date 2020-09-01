0dbc5784a3ef4eb2c88a445dc94aacf94617b4b4
JOAO meyer <joaomeyermuhlmann@gmail.com>
2020-08-31 21:49:57 -0300

    torricelli

--- Server.py
+++ Server.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask import render_template, session, redirect, url_for, request, flash, jsonify, Flask
 from datetime import datetime
 from MruCalculator import calculadora
+from TorricelliCalculator import calculadora
 
 
 app = Flask(__name__)
@@ -19,8 +20,21 @@ def index():
 def mru():
     return render_template('mru.html')
 
+@app.route('/toricelli')
+def torricelli():
+    return render_template('torriceli.html')
+
 @app.route('/calc', methods=['POST'])
 def calc():
+    if request.method == 'POST':
+        data = request.get_json()
+        if len(data):
+            resp = calculadora(data[0]["meu_campo1"], data[0]["meu_campo2"], data[0]["meu_campo3"])
+            return jsonify({"calculo": resp})
+    return redirect(url_for("index"))
+
+@app.route('/calc2', methods=['POST'])
+def calc2():
     if request.method == 'POST':
         data = request.get_json()
         if len(data):
@@ -39,7 +53,7 @@ def internal_server_error(e):
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', use_reloader=False)
-    app.run(debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
 
 
 
---
+++ TorricelliCalculator.py
@@ -0,0 +1,13 @@
+import math
+
+
+def calculadora(V0, a, s):
+    #s=Deslocamento(Delta s)
+    return math.sqrt(int(V0)**2+2*int(a)*int(s))
+
+
+
+
+
+
+
